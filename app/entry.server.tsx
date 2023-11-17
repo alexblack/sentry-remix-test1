8f24c6325e2369c42a0842581a5dc70e00bc096c
Alex Black <alex@alexblack.ca>
2023-11-17 09:52:58 -0800

    run sentry wizard

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/remix";
 /**
  * By default, Remix will handle generating the HTTP Response for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
@@ -6,11 +7,21 @@
 
 import { PassThrough } from "node:stream";
 
-import type { AppLoadContext, EntryContext } from "@remix-run/node";
+import type { AppLoadContext, DataFunctionArgs, EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+
+export function handleError(error: unknown, { request }: DataFunctionArgs) {
+  Sentry.captureRemixServerException(error, 'remix.server', request);
+}
+
+Sentry.init({
+  dsn: "https://b55344c5f7ec45d193fc8cf952460b63@o522466.ingest.sentry.io/5880196",
+  tracesSampleRate: 1,
+  environment: process.env.NODE_ENV,
+})
 
 const ABORT_DELAY = 5_000;
 
